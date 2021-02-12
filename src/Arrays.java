061acb731ef5ee73a6c4777ca83a935a1819b95c
crdiam <angiziewska@gmail.com>
2021-02-12 11:50:22 +0100

    Printing numbers divided by 3 using decrementation

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        int arrayLength = 100;
+
+        for (int i=arrayLength; i>0; i--) {
+            if (i%3==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
