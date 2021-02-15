5719440dd40bf048d866cd5a0aa207f4b151ed15
crdiam <angiziewska@gmail.com>
2021-02-15 14:52:28 +0100

    Constructor super exercises (App, AndroidApp, iPhoneApp)

---
+++ src/AppTester.java
@@ -0,0 +1,10 @@
+public class AppTester {
+
+    public static void main(String[] args) {
+        iPhoneAPp iApp = new iPhoneAPp("iApka");
+        iApp.runiPhoneApp();
+
+        AndroidApp anApp = new AndroidApp("chinese Apka");
+        anApp.runAndroidApp();
+    }
+}
---
+++ src/iPhoneAPp.java
@@ -0,0 +1,11 @@
+public class iPhoneAPp extends App{
+
+
+    public iPhoneAPp(String name) {
+        super(name);
+    }
+
+    public void runiPhoneApp() {
+        System.out.println(name+": Running iPhoneApp...");
+    }
+}
